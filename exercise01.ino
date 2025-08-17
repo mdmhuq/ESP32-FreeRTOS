e55024f4da01d4b1906739952a0224bd17456856
MD Muhtasimul Huq <121406023+mdmhuq@users.noreply.github.com>
2025-08-16 22:23:01 -0400

    Create exercise01.ino
    exercise 1

---
+++ exercise01.ino
@@ -0,0 +1,35 @@
+#if CONFIG_FREERTOS_UNICORE
+static const BaseType_t app_cpu=0;
+#else
+static const BaseType_t app_cpu=1;
+#endif
+
+static const int led_pin = LED_BUILTIN;
+
+
+void taskBlinkLED(void *parameter){
+ while(1){
+    digitalWrite(led_pin, HIGH);
+    vTaskDelay(700/portTICK_PERIOD_MS);
+    digitalWrite(led_pin, LOW);
+    vTaskDelay(300/portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+ pinMode(led_pin, OUTPUT);
+
+  xTaskCreatePinnedToCore(taskBlinkLED, 
+    "Toggle LED", 
+    1024, 
+    NULL, 
+    1, 
+    NULL, 
+    app_cpu
+  );
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
